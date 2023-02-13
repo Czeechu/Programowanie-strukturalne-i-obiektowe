f55010faa78191daf87483c58cee661eecf83b09
Czeechu <112950394+Czeechu@users.noreply.github.com>
2023-02-13 13:49:08 +0100

    Add files via upload

---
+++ for_each/for_each/for_each.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>     // std::cout
+#include <algorithm>    // std::for_each
+#include <vector>       // std::vector
+
+void myfunction(int i) {    //function
+    std::cout << ' ' << i;
+}
+
+struct myclass {            // fucntion object type:
+    void operator() (int i) { std::cout << ' ' << i; }
+} myobjetct;
+
+int main()
+{
+    std::vector<int> v{ 10, 20, 30 };
+
+    std::cout << "v: ";
+    for_each(v.begin(), v.end(), myfunction);
+    std::cout << '\n';
+
+    // or:
+    std::cout << "v: ";
+    for_each(v.begin(), v.end(), myobjetct);
+    std::cout << '\n';
+
+    return 0;
+}
